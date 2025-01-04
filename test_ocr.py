4878db030b3337f1c121a0e9d73f7d31e6e7dfd5
JerryYao80 <98963023+JerryYao80@users.noreply.github.com>
2025-01-04 23:30:15 +0800

    测试gemini OCR能力

---
+++ test_ocr.py
@@ -0,0 +1,8 @@
+from google import genai
+from google.genai import types
+import requests
+import pathlib
+
+client = genai.Client(api_key='AIzaSyCfC90VhSlMX_j3wGsZnjHuze0e0pSJefM')
+MODEL_ID = "gemini-2.0-flash-exp" 
+
